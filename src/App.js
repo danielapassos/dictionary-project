213b4dc63e3f284fca4e2fb14fdb474f3556303e
Daniela Passos <moitidani@gmail.com>
2022-09-28 22:28:54 +0300

    Included a simple form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './crap.jpeg';
-import './App.css';
+import logo from "./crap.jpeg";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Project in <i>development</i>.
-        </p>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Project in <i>development</i>.
+          </p>
+        </header>
 
-      </header>
+        <main>
+          <Dictionary/>
+        </main>
+
+        <footer className="text-center">
+          <p>Project coded by Daniela Passos</p>
+        </footer>
+      </div>
     </div>
   );
 }
