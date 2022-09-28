b4c784d629a1508b6a160a55cc521dad3e240a9f
Daniela Passos <moitidani@gmail.com>
2022-09-28 20:52:54 +0300

    Foundation

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './crap.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Project in <i>development</i>.
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
