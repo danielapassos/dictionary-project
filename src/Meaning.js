390b0d3366cbf30e2a85e87a738ea2f189405ca1
Daniela Passos <moitidani@gmail.com>
2022-10-02 19:26:23 +0200

    Fixed code bugs

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,27 +1,24 @@
 import React from "react";
-import Synonyms from "./Synonyms"
+import Synonyms from "./Synonyms";
 
-
-export default function Meaning (props){
-
-    return (
-        <div className="Meaning">
-            <h3>{props.meaning.partOfSpeech}</h3>
-            {props.meaning.definitions.map(function(definition,index){
-                return(
-                    <div key={index}>
-                        <p>
-                            <strong>Definition:</strong> {definition.definition}
-                            <br/>
-                            <strong>Example:</strong> {definition.example}
-                            <br/>
-                            <Synonyms synonyms={definition.synonyms}/>
-                        </p>
-                    </div>
-                );
-            })}
-            <p>{props.meaning.definitions[0].definition}</p>
-            <p>{props.meaning.definitions[0].example}</p>
-        </div>
-    )
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition:</strong> {definition.definition}
+              <br />
+              <strong>Example:</strong>
+              <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
